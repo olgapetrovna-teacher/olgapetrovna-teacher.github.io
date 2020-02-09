e5d3116ed2544adc69280f3275ffb231fb74b8e3
taurus1605 <54315228+taurus1605@users.noreply.github.com>
2020-02-09 11:34:45 +0300

    responsive for 1199px

---
+++ master/olgapetrovna/js/script.js
@@ -0,0 +1,13 @@
+//if(innerWidth < 992) {
+//    $(".navbar_item").removeClass("col-md-4").addClass("col-md-6");
+//    
+//}
+//
+//
+//
+//window.addEventListener("resize", function() {
+//    if(innerWidth < 992) {
+//        $(".navbar_item").removeClass("col-md-4").addClass("col-md-6");
+//    }
+//    
+//}, false);
