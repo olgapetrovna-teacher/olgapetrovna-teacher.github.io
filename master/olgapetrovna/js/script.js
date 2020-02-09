1677352d933b2dd0b34cd3beef56b52afdbbfd9e
taurus1605 <54315228+taurus1605@users.noreply.github.com>
2020-02-09 13:50:39 +0300

    Responsive page

--- master/olgapetrovna/js/script.js
+++ master/olgapetrovna/js/script.js
@@ -1,13 +1,37 @@
-//if(innerWidth < 992) {
-//    $(".navbar_item").removeClass("col-md-4").addClass("col-md-6");
-//    
-//}
-//
-//
-//
-//window.addEventListener("resize", function() {
-//    if(innerWidth < 992) {
-//        $(".navbar_item").removeClass("col-md-4").addClass("col-md-6");
-//    }
-//    
-//}, false);
+function replaceCreedAndNavbarItem1() {
+    navbar_item_1 = $("#navbar_item_1")
+    creed = $("#navbar_creed");
+    $(".navbar").children().first().replaceWith(creed);
+    $("#navbar_creed").after(navbar_item_1);
+}
+
+function replaceNavbarItem1AndCreed() {
+    navbar_item_1 = $("#navbar_item_1")
+    creed = $("#navbar_creed");
+    $(".navbar").children().first().replaceWith(navbar_item_1);
+    $("#navbar_item_1").after(creed);
+}
+
+if(innerWidth < 992) {
+    replaceCreedAndNavbarItem1();
+}
+if(innerWidth < 531) {
+    $(".container").addClass("wrapper");
+    $(".wrapper").removeClass("container");
+}
+
+window.addEventListener("resize", function() {
+    if(innerWidth < 992) {
+        replaceCreedAndNavbarItem1();
+    }
+    else if(innerWidth >= 992) {
+        replaceNavbarItem1AndCreed();
+    }
+    if(innerWidth < 531) {
+        $(".container").addClass("wrapper");
+        $(".container").removeClass("container");
+    } else {  
+        $(".wrapper").addClass("container");
+        $(".container").removeClass("wrapper");
+    }
+});
